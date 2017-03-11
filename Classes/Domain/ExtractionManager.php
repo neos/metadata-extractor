d0d55be2f84a7a84e3eb19a03cf2a8455c101155
Daniel Lienert <daniel@lienert.cc>
2017-03-11 14:03:54 +0100

    TASK: Apply migration TYPO3.Flow-20161115140430
    Adjust to the renaming of the Object namespace in Flow 4.0

--- Classes/Domain/ExtractionManager.php
+++ Classes/Domain/ExtractionManager.php
@@ -18,7 +18,7 @@ use Neos\MetaData\Extractor\Exception\ExtractorException;
 use Neos\MetaData\MetaDataManager;
 use TYPO3\Media\Domain\Model\ImageVariant;
 use TYPO3\Flow\Annotations as Flow;
-use TYPO3\Flow\Object\ObjectManager;
+use TYPO3\Flow\ObjectManagement\ObjectManager;
 use TYPO3\Flow\Reflection\ReflectionService;
 use TYPO3\Flow\ResourceManagement\PersistentResource as FlowResource;
 use TYPO3\Media\Domain\Model\Asset;
